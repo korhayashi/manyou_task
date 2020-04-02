8cc043c3ee3f66a4aee63c842e7376b8b99d6a1e
kaori hayashi <suzu_33@hotmail.com>
2020-04-02 23:20:58 +0900

    task新規等機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,19 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
-
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -21,5 +28,11 @@ class TasksController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :detail)
   end
 end
